5aae0c6126ee8f274445bdf54ea5867609248c66
Phoenix591 <11486+Phoenix591@users.noreply.github.com>
2018-05-07 21:43:19 -0500

    minor tweak to include in recovery

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := src/zip.c src/zipfile.c src/zipup.c src/fileio.c src/util.c src/globals.c \
                    src/crypt.c src/ttyio.c src/crc32.c src/zbz2err.c src/deflate.c \
-                   src/trees.c src/unix/unix.c 
+                   src/trees.c src/unix/unix.c
 
 LOCAL_C_INCLUDES := src src/unix
 
@@ -15,8 +15,7 @@ LOCAL_CFLAGS += -Wno-error
 LOCAL_ARM_MODE := arm
 
 LOCAL_MODULE := zip
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
-
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
+LOCAL_LDFLAGS += -Wl,-dynamic-linker,/sbin/linker64
 include $(BUILD_EXECUTABLE)
 
