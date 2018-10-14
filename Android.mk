40e900b21e3db3c26ca78e9bcd2c79602013b42f
Phoenix591 <11486+Phoenix591@users.noreply.github.com>
2018-10-14 01:40:00 -0500

    rename to zip-r to avoid conflict

--- Android.mk
+++ Android.mk
@@ -14,7 +14,7 @@ LOCAL_CFLAGS += -Wno-error
 
 LOCAL_ARM_MODE := arm
 
-LOCAL_MODULE := zip
+LOCAL_MODULE := zip-r
 LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
 LOCAL_LDFLAGS += -Wl,-dynamic-linker,/sbin/linker64
 include $(BUILD_EXECUTABLE)
